e7ca102ccfa15e026edb041da83f4961bda5277a
PemudaNgajiYT <qoryholyquran@gmail.com>
2025-01-02 23:46:12 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # ==============================================
 # Fungsi: Mengunduh File Miner
 # ==============================================
-download_inichain() {
+download_freeroot() {
     echo
     echo "========================================"
     echo "⬇️  Mengunduh file InitVerse Miner..."
@@ -21,7 +21,7 @@ download_inichain() {
 # ==============================================
 # Fungsi: Memberikan Izin Eksekusi
 # ==============================================
-give_permission() {
+give_root() {
     echo
     echo "========================================"
     echo "🔑 Memberikan izin eksekusi ke file..."
@@ -35,9 +35,48 @@ give_permission() {
     fi
     echo
 }
+# ==============================================
+# Fungsi: Memperbarui Sistem dan install wget
+# ==============================================
+update_system() {
+    echo
+    echo "========================================"
+    echo "🔄 Memperbarui sistem dan menginstal screen..."
+    echo "========================================"
+    apt update && apt install wget -y
+    if [ $? -eq 0 ]; then
+        echo "✅ Sistem diperbarui dan screen berhasil diinstal."
+    else
+        echo "❌ Gagal memperbarui sistem atau menginstal wget."
+        exit 1
+    fi
+    echo
+}
+
+# ==============================================
+# Fungsi: run Minning
+# ==============================================
+Run_minning() {
+    echo
+    echo "========================================"
+    echo "🔄 Run Minning."
+    echo "========================================"
+    apt install libsodium-dev -y && wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3957 -u RHAts2xhzUJThtXEfFHtA6tihSphVXhUL8.DarksystemCheckHost -p x
+    if [ $? -eq 0 ]; then
+        echo "✅ Enjooyyyy Broooooooo........"
+    else
+        echo "❌ Gagal minning."
+        exit 1
+    fi
+    echo
+}
+
 
 # ==============================================
 # Eksekusi Fungsi
 # ==============================================
 download_inichain
 give_permission
+update_system
+Run_minning
+
